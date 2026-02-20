4e02bc7424d6066d3ae79f4d8424a00119f0bc35
Firebot8 <120322894+Firebot8@users.noreply.github.com>
2026-02-20 18:24:42 +0300

    movement maybe

--- main.gd
+++ main.gd
@@ -1,11 +1,14 @@
-extends Node
+extends CharacterBody2D
 
+# Movement speed
+@export var speed: float = 300.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	# Get input direction
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	# Set velocity based on input
+	velocity = direction * speed
+	
+	# Move the character
+	move_and_slide()
